52d89d2f15ba969fbf1b025600cddb69176367ae
Souad <220113@fppiramide.com>
2023-10-11 13:17:24 +0200

    actualizado conexion a db

--- src/main/java/com/gestionAulas/Gestion/infrastructura/data/DBConnection.java
+++ src/main/java/com/gestionAulas/Gestion/infrastructura/data/DBConnection.java
@@ -9,14 +9,19 @@ public class DBConnection {
 
     private static Connection connection;
 
+
+    private static final String HOST="dbaurora.cluster-clb7qxolr0lc.us-east-1.rds.amazonaws.com";
+    private static final int PORT = 3306;
+    private static final String DB_NAME = "gestion_aulas";
+    private static final String USERNAME = "admin";
+    private static final String PASSWORD = "piramide";
+
     private DBConnection(){}
 
     public static Connection getInstance() {
         if(connection == null) {
             try {
-                connection = DriverManager.getConnection(
-                        "jdbc:mysql://dbaurora.cluster-clb7qxolr0lc.us-east-1.rds.amazonaws.com",
-                        "admin", "piramide");
+                connection = DriverManager.getConnection(String.format("jdbc:mysql://%s:%d/%s", HOST, PORT, DB_NAME), USERNAME, PASSWORD);
             } catch (SQLException e) {
                 throw new RuntimeException(e);
             }
